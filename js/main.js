c01124d69ff4f6389b446fd3eb4b09c65335955b
Ramiro Mangione <ramiromangionetello@gmail.com>
2024-05-22 14:51:59 -0300

    agregados objetos arrays y funciones

--- js/main.js
+++ js/main.js
@@ -1,12 +1,32 @@
 console.log("hola");
+class producto{
+    constructor (nombre, precio,){
+        this.nombre=nombre;
+        this.precio=precio;
+        this.cantidad=0;
+    }
+    agregar(pedido){
+        this.cantidad=this.cantidad+pedido
+    }
+}
 function compra(){
     let finCompra = false;
-    let carrito = 0
-   
+    let total = 0
+
+    const chocolate = new producto("Chocolate",100,0);
+    const agua = new producto("Agua",50,0);
+    const coca = new producto("Coca",80,0);
+    const pan = new producto("Pan",60,0);
+    const alfajor = new producto("Alfajor",110,0);
+    let carrito=[chocolate,agua,coca,pan,alfajor];
+
     while(finCompra === false){
-        const ingreso = parseInt( prompt("¿Que queres comprar hoy? \n1. Chocolate $100\n2. Agua $50\n3. Coca $80\n4. Pan $60\n5. Alfajor $110 \n\n0. Finalizar compra                                                  Carrito:$"+carrito));
+        const ingreso = parseInt( prompt("¿Que queres comprar hoy? \n1. Chocolate $100\n2. Agua $50\n3. Coca $80\n4. Pan $60\n5. Alfajor $110 \n\n0. Finalizar compra"));
         if (ingreso == 0){
-            alert("Su total a pagar es:$"+carrito)
+            for (let i = 0; i < carrito.length; i++) {
+            total = total + (carrito[i].precio*carrito[i].cantidad)
+        }
+            alert("Su total a pagar es:$"+total)
             finCompra = true;
             const volver = parseInt (prompt("Si quiere realizar otra compra ingrese 1"))
             if (volver == 1){
@@ -15,24 +35,28 @@ function compra(){
         }
         if (ingreso == 1){
             const cantidad = parseInt(prompt("Ingrese cuantas unidades quiere"))
-            carrito = carrito+100*cantidad
+            chocolate.agregar(cantidad)
         }
         if (ingreso == 2){
             const cantidad = parseInt(prompt("Ingrese cuantas unidades quiere"))
-            carrito = carrito+50*cantidad
+            agua.agregar(cantidad)
         }
         if (ingreso == 3){
             const cantidad = parseInt(prompt("Ingrese cuantas unidades quiere"))
-            carrito = carrito+80*cantidad
+            coca.agregar(cantidad)
         }
         if (ingreso == 4){
             const cantidad = parseInt(prompt("Ingrese cuantas unidades quiere"))
-            carrito = carrito+60*cantidad
+            pan.agregar(cantidad)
         }
         if (ingreso == 5){
             const cantidad = parseInt(prompt("Ingrese cuantas unidades quiere"))
-            carrito = carrito+110*cantidad
+            alfajor.agregar(cantidad)
         }
     }
+    console.log("detalle de su pedido")
+    for (let i = 0; i < carrito.length; i++) {
+        console.log(carrito[i].nombre+"  $"+carrito[i].precio+"  "+carrito[i].cantidad)
+    }
 };
-compra();
+    compra();
